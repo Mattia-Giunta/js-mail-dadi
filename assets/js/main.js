9be2c0642f6911c51675947ac3e3e369c2c0dd92
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-01-10 22:21:16 +0100

    terminato esercizio controllo email

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,7 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-
-// **Mail**
-// Chiedi all’utente la sua email,
-// controlla che sia nella lista di chi può accedere,
-// stampa un messaggio appropriato sull’esito del controllo.
 // **Consigli del giorno:**
-
-
 // 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 // 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
 // 3. si ma noi cosa vogliamo fare?
@@ -22,7 +15,7 @@
 
 // creazione gioco dei dadi
 
-// funzione custom
+// funzione custom per numero random
 function randomNumber( min , max ){
 
     return Math.floor(Math.random() * ( max - min + 1 ) + min )
@@ -52,12 +45,17 @@ const drawHTML = document.getElementById('draw');
 // funzione al click
 btnPlayHTML.addEventListener('click', function(){
 
+    // variabile per il random del giocatore
     let playerNumber = randomNumber(1, 6);
+    
+    // variante per il random del computer
     let computerNumber = randomNumber(1, 6);
     
+    // reset della schermata dopo ogni click
     drawHTML.classList.add('d-none');
     winHTML.classList.add('d-none');
     loseHTML.classList.add('d-none');
+
 
     if ( playerNumber === computerNumber ){
 
@@ -82,4 +80,55 @@ btnPlayHTML.addEventListener('click', function(){
 // Terminato esercizio dei dadi
 
 
+
+// **Mail**
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
 // Creazione controllo mail
+
+
+const containerMailHTML = document.getElementById('containerMail');
+
+const btnSubmitHTML = document.getElementById('btnSubmit');
+
+
+btnSubmitHTML.addEventListener('click', function(){
+
+    // array delle mail
+    const listaMail = ["mail1@hotmail.com", "mail2@hotmail.com", "mail3@hotmail.com", "mail4@hotmail.com", "mail5@hotmail.com"];
+
+    // costante per la text area
+    let inputEmailHTML = document.getElementById('InputEmail');
+
+    // variabile booleana
+    let mailControl = false;
+
+    for (let i = 0; i < listaMail.length; i++){
+
+        if (inputEmailHTML.value === listaMail[i]){
+
+            mailControl = true;
+
+            break;
+
+        } 
+
+    }
+
+    if ( mailControl ){
+
+        containerMailHTML.innerHTML += '<p class= "text-success fs-3 "> Ben tornato!!! Puoi accedere al tuo profilo </p>';
+
+    } else {
+
+        alert("Non sei registrato, riprova con un'altra Mail");
+
+    }       
+    
+        
+}
+)
+// terminato esercizio controllo email
